5e6c40f209d90d3426a6e33da2dceea0af93ae3e
James Lund <lundj@eos02.cis.gvsu.edu>
2019-09-11 21:50:45 -0400

    Final editions to shell.

---
+++ shell.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/resource.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+  while(1){
+    char input[30];
+    char *parse;
+    struct rusage stats;
+    char *token[10];
+    int commandLen;
+
+    pid_t pid;
+    int status;
+  
+    printf("$> ");
+    fgets(input, 30, stdin);
+    commandLen = strlen(input);
+    input[commandLen - 1] = '\0';
+  
+    parse = strtok(input, " ");
+    for(int i = 0; i < commandLen; i++){
+	token[i] = parse;
+	parse = strtok(NULL, " ");
+    }
+
+    if(strcmp(input, "quit") == 0){
+	break;
+    }
+  
+    if ((pid = fork()) < 0){
+	perror("fork failure");
+	exit(1);
+    }
+    
+    else if (pid == 0){
+      if (execvp(token[0], token)) {
+        perror("exec failed");
+        exit(1);
+      }
+    }
+    
+    else {
+	wait(&status);
+	getrusage(RUSAGE_CHILDREN, &stats);
+	printf("Cpu time used: %ld.%06ld sec\n", stats.ru_utime.tv_sec, stats.ru_utime.tv_usec);
+	printf("Context Switches: %ld\n", stats.ru_nivcsw);
+    }
+  }
+  printf("Exiting shell..\n");
+  return 0;
+}
+
